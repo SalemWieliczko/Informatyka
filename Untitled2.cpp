9995fc5400b811ca4c007ec351e03dee63b38fce
SalemWieliczko <salem.l.e.sko@gmail.com>
2025-02-19 12:46:53 +0100

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a = 6;
+	int b = 4;
+	
+	int suma = a + b;
+	int roznica = a - b;
+	int iloczyn = a * b;
+	
+	cout << "Liczba a: " << a<<endl;
+	
+	cout << "Liczba b: " <<b<<endl;
+	
+	cout << "Suma: " << suma<<endl;
+	
+	cout << "Roznica: " << roznica<<endl;
+	
+	cout <<"Iloczyn: " << iloczyn<<endl;
+	
+	
+    return 0;
+}
