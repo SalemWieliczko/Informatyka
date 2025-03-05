d88c3ce820204b38cd340a5e5757a4b23b14a835
SalemWieliczko <salem.l.e.sko@gmail.com>
2025-03-05 13:13:22 +0100

    Add files via upload

---
+++ TablicaZnak1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	char ciag[21];
+	
+	cout << "Podaj ciag znakow (maksymalnie 20):";
+	cin.getline(ciag, 21);
+	cout << "Wprowadzony ciag znakow:" << ciag <<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+}
+
+
