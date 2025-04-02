44e33b0bb7381e910e79c685be6c5a87bb4cffc4
SalemWieliczko <salem.l.e.sko@gmail.com>
2025-04-02 13:14:54 +0200

    Add files via upload

---
+++ Funkcje1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+ //zrob z tego funkcje
+int Sprawdz()
+ 
+int main() {
+    int N;
+ 
+    cout << "Podaj liczbe caukowita: ";
+    cin >> N;
+
+
+
+        if (N % 2 != 0) {  
+            cout << "Liczba "<< N <<" nie jest parzysta."<<endl;
+	    } else {
+	    	cout << "Liczba "<< N <<" jest parzysta."<<endl;
+		}
+   
+ 
+    cout << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
