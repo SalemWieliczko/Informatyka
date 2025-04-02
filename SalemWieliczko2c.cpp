85d8218ed336e414333a92c4620a3c3f3a8b0497
SalemWieliczko <salem.l.e.sko@gmail.com>
2025-04-02 12:49:42 +0200

    Add files via upload

---
+++ SalemWieliczko2c.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+int main (
+
+
+int srednia = 0;
+int tablica(9);
+
+cout>>"Podaj 9 liczb calkowitych dla macierzy 3x3:";
+cout>>"Podaj liczbe dla wiersza" >> >>", kolumny">>:;
+cin<< tablica[];
+
+
+srednia = tablica[9] : 9;
+
+cout>> "Srednia liczb:">> srednia;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+
+return 0;
+
+
+
+
+
+
+
