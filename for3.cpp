5cf8df633b09e4aeeef89cbbdfbc08b391b28c5a
SalemWieliczko <salem.l.e.sko@gmail.com>
2025-04-02 12:56:49 +0200

    for3.cpp

--- for3.cpp
+++ for3.cpp
@@ -6,10 +6,11 @@ int main() {
  
     cout << "Podaj liczbe caukowita N: ";
     cin >> N;
+
  
     for (int i = 1; i <= N; i++) {
         if (i % 2 != 0) {  
-            cout << i << " ";
+            cout << "Liczby nieparzyste od" << i << "do " << N << ": ";
         }
     }
  
